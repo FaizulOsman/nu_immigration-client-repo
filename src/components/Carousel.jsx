6e9183ccfdc4db669a4f4fa40f9a130d0f59788e
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-10 19:58:56 +0600

    react awesome slider added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,56 +3,27 @@ import banner1 from "../assets/images/banner1.jpg";
 import banner2 from "../assets/images/banner2.jpg";
 import banner3 from "../assets/images/banner3.jpg";
 import banner4 from "../assets/images/banner4.jpg";
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
+import "./Carousel.css";
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Carousel = () => {
   return (
     <div>
-      <div className="carousel w-full duration-75">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src={banner1} className="w-full" alt="img" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src={banner2} className="w-full" alt="img" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src={banner3} className="w-full" alt="img" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src={banner4} className="w-full" alt="img" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-      </div>
+      <AutoplaySlider
+        className="aws-btn"
+        play={true}
+        cancelOnInteraction={false} // should stop playing on user interaction
+        interval={3000}
+      >
+        <div data-src={banner1} />
+        <div data-src={banner2} />
+        <div data-src={banner3} />
+        <div data-src={banner4} />
+      </AutoplaySlider>
     </div>
   );
 };
